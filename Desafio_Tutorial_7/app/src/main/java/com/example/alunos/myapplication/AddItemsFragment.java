618193767cbf5ae09076ae8bf541dc79d37d6497
Rayane <ray@hotmail.com>
2018-05-07 10:41:46 -0300

    Tutorial 7

--- Desafio_Tutorial_7/app/src/main/java/com/example/alunos/myapplication/AddItemsFragment.java
+++ Desafio_Tutorial_7/app/src/main/java/com/example/alunos/myapplication/AddItemsFragment.java
@@ -1,19 +1,51 @@
 package com.example.alunos.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.alunos.myapplication.model.Pessoa;
+
+import java.util.ArrayList;
 
 public class AddItemsFragment extends Fragment{
+
+    EditText editNome;
+
+    ArrayList<Pessoa> lista = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState){
+                             ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_add_items_layout,
                 container, false);
         return v;
+    }
+        public void salvar(View v){
+
+        editNome = (EditText)findViewById(R.id.editName);
+
+        SharedPreferences arquivo = getPreferences(Context.MODE_PRIVATE);
+        String name = editNome.getText().toString();
+        if(name.matches("")  ){
+            Toast toast = Toast.makeText(AddItemsFragment.this,
+                    "Digite algo...", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        SharedPreferences.Editor editor = arquivo.edit();
+        editor.putString("nome", name);
+        editor.commit();
+        editNome.setText("");
+
+        lista.add(new Pessoa(name));
     }
 }
--- Desafio_Tutorial_7/app/src/main/java/com/example/alunos/myapplication/ViewItensFragment.java
+++ Desafio_Tutorial_7/app/src/main/java/com/example/alunos/myapplication/ViewItensFragment.java
@@ -1,5 +1,6 @@
 package com.example.alunos.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,5 +16,10 @@ public class ViewItensFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_view_items_layout,
                 container, false);
         return v;
+        Intent it = new Intent(this, mostraListaDinamica.class);
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList("contatos", lista);
+        it.putExtras(bundle);
+        startActivity(it);
     }
 }
