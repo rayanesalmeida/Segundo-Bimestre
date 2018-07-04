26de22546aa345dc1612d3b14e32d583ff689178
Rayane <ray@hotmail.com>
2018-07-04 08:37:36 -0300

    Desafio Tutorial 9

--- Desafio_Tutorial_9/app/src/main/java/com/example/alunos/myapplication/MainActivity.java
+++ Desafio_Tutorial_9/app/src/main/java/com/example/alunos/myapplication/MainActivity.java
@@ -22,8 +22,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText tnome = new EditText(this);
-    EditText ttelefone = new EditText(this);
+    EditText tnome;
+    EditText ttelefone;
 
     ArrayList<Pessoa> lista = new ArrayList<>();
 
@@ -33,21 +33,17 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //Cria o layout
-
         TableLayout tabela = new TableLayout(this);
         tabela.setPadding(10, 10, 10, 10);
         tabela.setLayoutParams(new LayoutParams(
                 LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
-
-        //Expande a coluna 1
         tabela.setColumnStretchable(1, true);
         //Linha 1
         TableRow linha1 = new TableRow(this);
         TextView nome = new TextView(this);
         nome.setText("Nome: ");
         linha1.addView(nome);
-        //Focus
+        tnome = new EditText(this);
         tnome.requestFocus();
         linha1.addView(tnome);
 
@@ -56,15 +52,11 @@ public class MainActivity extends AppCompatActivity {
         TextView telefone = new TextView(this);
         telefone.setText("Telefone: ");
         linha2.addView(telefone);
-        //ttelefone.setTransformationMethod(
-                //new NumberFormatException());
+        ttelefone = new EditText(this);
         linha2.addView(ttelefone);
-
-        //Linha 3
 
         TableRow linha3 = new TableRow(this);
         linha3.setGravity(Gravity.CENTER_HORIZONTAL);
-        //Botão alinhado a direita
         Button salvar = new Button(this);
         salvar.setText("Salvar ");
         salvar.setOnClickListener(new View.OnClickListener() {
@@ -94,48 +86,22 @@ public class MainActivity extends AppCompatActivity {
         linha4.setGravity(Gravity.CENTER_HORIZONTAL);
         Button carregar = new Button(this);
         carregar.setText("Carregar ");
+        carregar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                                            Intent it = new Intent(getBaseContext(), Main2Activity.class);
+                                            Bundle bundle = new Bundle();
+                                            bundle.putParcelableArrayList("contatos", lista);
+                                            it.putExtras(bundle);
+                                            startActivity(it);
+            }
+        });
         linha4.addView(carregar);
 
-        //Adiciona as linhas
         tabela.addView(linha1);
         tabela.addView(linha2);
         tabela.addView(linha3);
         tabela.addView(linha4);
 
-        //Informa o layout
         setContentView(tabela);
     }
 }
-    /*public void salvar(View V){
-        SharedPreferences arquivo = getPreferences(Context.MODE_PRIVATE);
-        String name = tnome.getText().toString();
-        String telefone  = ttelefone.getText().toString();
-        if(name.matches("") && telefone.matches("") ){
-            Toast toast = Toast.makeText(MainActivity.this,
-                    "Digite algo...", Toast.LENGTH_SHORT);
-            toast.show();
-            return;
-        }
-        SharedPreferences.Editor editor = arquivo.edit();
-        editor.putString("nome", name);
-        editor.putString("telefone", telefone);
-        editor.commit();
-        tnome.setText("");
-        ttelefone.setText("");
-
-        lista.add(new Pessoa(name, telefone));
-    }
-
-    @Override
-    public void onClick(View v){
-        salvar();
-    }
-
-    public void mostrarLista(View v){
-        Intent it = new Intent(this, Main2Activity.class);
-        Bundle bundle = new Bundle();
-        bundle.putParcelableArrayList("contatos", lista);
-        it.putExtras(bundle);
-        startActivity(it);
-    }
-}*/
